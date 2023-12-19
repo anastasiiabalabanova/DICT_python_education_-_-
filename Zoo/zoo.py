3f665f7947e1377f577ec8602dc93be203a86b9b
Anastasiia Balabanova <a.d.balabanova@student.khai.edu>
2023-12-19 22:03:31 +0200

    Zoo 2-nd stage

--- Zoo/zoo.py
+++ Zoo/zoo.py
@@ -5,3 +5,135 @@ print("Let's check out the animals...")
 print("The deer looks fine.")
 
 print("The lion looks healthy.")
+
+print("camel = 1, rabbit = 2, deer = 3, lion = 4, goose = 5,  bat = 6")
+
+camel = r"""
+The camel habitat...
+
+ ___.-''''-.
+/___  @    |
+',,,,.     |          _.'''''''._
+     '     |         /           \
+     |     \    _.-'              \
+     |      '.-'                   '-.
+     |                                ',
+     |                                 '',
+      ',,-,                            ':;
+           ',,| ;,,                  ,' ;;
+              ! ; !'',,,',',,,,'!   ;   ;:
+             : ;  ! !       ! ! ;   ;   :;
+             ; ;   ! !      ! !  ; ;    ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that!"""
+
+lion = r"""
+The lion habitat...
+
+                                               ,w.
+                                             ,YWMMw ,M ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW" @\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+ "^MP__.-'   ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \ :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is roaring!"""
+
+deer = r""" 
+The deer habitat... 
+    /| |\
+ `__\\ //__'
+    || ||
+ \__`\ |'__/
+  `_\\ //_'
+ _.,:---;,._
+ \_:     :_/
+   |@. .@|
+   |     |
+   ,\.-./ \
+   ;;`-'   `---__________-----.-.
+   ;;;                         \_\
+   ';;;                         |
+    ;    |                      ;
+     \    \    \        |      /
+      \_,  \   /        \     |\
+        |'; |  |,,,,,,,,/ \   \ \_
+        |   |  |           \  /   |
+         \  \  |           | / \  |
+          | || |           | |  | |
+          | || |           | |  | | 
+          | || |           | |  | |
+          | || |           | |  | |
+          |_||_|           |_|  |_|
+Pretty good! 
+--- 
+You've reached the end of the program."""
+
+goose = r""" 
+The goose habitat... 
+                              _
+                         ,- "" "".
+                       , '   ____  `.
+                     , ' ,'      `. `._
+  (`.     _..--.._ ,'  ,'          \   \
+ (`-.\  .-""     ""'  /            ( d _b
+(`._ ` -"" ,._        (             `-(  \
+<_ `     ( <`<        \                `-._\
+ <`-      (__< <       :
+  (__      (_<_<       ;
+    `------------------------------------------               
+Beautiful! 
+--- 
+You've reached the end of the program."""
+
+bat = r""" 
+The bat habitat... 
+_____________            _____________
+~-.           \ |\___/| /          .-~ 
+  ~-.         \ / o o \ /       .-~ 
+      >        \\  W  //        < 
+     /          /~---~\         \ 
+    /_         |        |       _\ 
+      ~-.      |        |    .-~ 
+         ;      \      /     i 
+        /___    /\    /\    ___\ 
+           ~-. /  \_/   \ .-~ 
+              V           V 
+It's doing fine. 
+--- 
+You've reached the end of the program."""
+
+rabbit = r""" 
+The rabbit habitat... 
+           , 
+          /|     __ 
+         / |   ,-~ / 
+        Y :|  // / 
+       | jj /( .^ 
+       >-"~"-v" 
+       /        Y 
+      jo o      | 
+     ( ~T~     j 
+      >._-'  _./ 
+     /  "~"    | 
+    Y      _,  | 
+  /|  ;-"~ _   l 
+/ l/  ,-"~      \ 
+\//\/         .- \ 
+ Y         /      Y 
+ l         I      ! 
+ ]\         _\   /"\ 
+(" ~----( ~   Y.    ) 
+It looks fine! 
+--- 
+You've reached the end of the program."""
